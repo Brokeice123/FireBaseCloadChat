16f99595811c05eb70dc2569f743638ab0799264
Brokeice123 <keithmarkomen@gmail.com>
2023-04-26 13:19:52 +0300

    Added logic for creating user in Register Page

--- app/src/main/java/com/example/firecloudchat_app/RegisterActvity.kt
+++ app/src/main/java/com/example/firecloudchat_app/RegisterActvity.kt
@@ -3,6 +3,9 @@ package com.example.firecloudchat_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
 
 class RegisterActvity : AppCompatActivity() {
 
@@ -10,6 +13,11 @@ class RegisterActvity : AppCompatActivity() {
     lateinit var regEmail:EditText
     lateinit var regPassword:EditText
     lateinit var regCreateAcc:EditText
+
+    //Initialise Firebase
+    lateinit var auth: FirebaseAuth
+    
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +27,26 @@ class RegisterActvity : AppCompatActivity() {
         regEmail = findViewById(R.id.edtemailreg)
         regPassword = findViewById(R.id.edtpassreg)
         regCreateAcc = findViewById(R.id.btncreateaccount)
+        
+        auth = FirebaseAuth.getInstance()
 
         regCreateAcc.setOnClickListener {
+            var name = regFullname.text.toString().trim()
+            var email = regEmail.text.toString().trim()
+            var password = regPassword.text.toString().trim()
+            
+            //Validate your Input
+            if (name.isEmpty() || email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "One of the fields is empty", Toast.LENGTH_SHORT).show()
+            } else{
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this){
+                    if (it.isSuccessful){
+                        Toast.makeText(this, "User Created Successfully", Toast.LENGTH_SHORT).show()
+                    } else{
+                        Toast.makeText(this, "Failed to Create Account", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
 
         }
 

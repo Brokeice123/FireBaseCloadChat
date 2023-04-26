1bb99240c7f9d1137a4282b69d6f2a27ff0981ae
Brokeice123 <keithmarkomen@gmail.com>
2023-04-26 13:36:31 +0300

    Logic for signing in user in Login Activity

--- app/src/main/java/com/example/firecloudchat_app/LoginAtivity.kt
+++ app/src/main/java/com/example/firecloudchat_app/LoginAtivity.kt
@@ -1,9 +1,12 @@
 package com.example.firecloudchat_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginAtivity : AppCompatActivity() {
 
@@ -11,6 +14,9 @@ class LoginAtivity : AppCompatActivity() {
     lateinit var mPassword:EditText
     lateinit var mbtnLogin:Button
     lateinit var mbtnRegister:Button
+    
+    //Initialise Firebase
+    lateinit var auth:FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +26,30 @@ class LoginAtivity : AppCompatActivity() {
         mPassword = findViewById(R.id.edtPassword)
         mbtnLogin = findViewById(R.id.btnLogin)
         mbtnRegister = findViewById(R.id.btnRegister)
+        
+        auth = FirebaseAuth.getInstance()
 
         mbtnLogin.setOnClickListener {
-
+            
+            var email = mEmail.text.toString().trim()
+            var password = mPassword.text.toString().trim()
+            
+            //Validate Input
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "One of the inputs is empty", Toast.LENGTH_SHORT).show()
+            } else{
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) {
+                    if (it.isSuccessful){
+                        Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                        var gotomain = Intent(this, MainActivity::class.java)
+                        startActivity(gotomain)
+                        finish()
+                    } else{
+                        Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            
         }
 
         mbtnRegister.setOnClickListener {

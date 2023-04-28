d7b12bd095205fedda275965ea6d8af0b0a1f68d
Brokeice123 <keithmarkomen@gmail.com>
2023-04-28 12:38:27 +0300

    Login Page Done

--- app/src/main/java/com/example/firecloudchat_app/LoginAtivity.kt
+++ app/src/main/java/com/example/firecloudchat_app/LoginAtivity.kt
@@ -1,12 +1,15 @@
 package com.example.firecloudchat_app
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import androidx.core.graphics.ColorUtils
+import android.view.WindowManager
 
 class LoginAtivity : AppCompatActivity() {
 
@@ -58,6 +61,12 @@ class LoginAtivity : AppCompatActivity() {
             finish()
         }
 
+        val color1 = Color.parseColor("#80b98fd7") // Replace with your first color
+        val color2 = Color.parseColor("#60bbc9fc") // Replace with your second color
+        val ratio = 0.125f // Replace with the ratio calculated from your angle
+        val blendedColor = ColorUtils.blendARGB(color1, color2, ratio)
+
+        window.statusBarColor = blendedColor
 
     }
 }

a9fcf564b3a35d80daa40fe3c533000aa39c9484
Brokeice123 <keithmarkomen@gmail.com>
2023-04-28 07:41:48 +0300

    Finished Splash Screen and login for Buttons

--- app/src/main/java/com/example/firecloudchat_app/SplashScreen.kt
+++ app/src/main/java/com/example/firecloudchat_app/SplashScreen.kt
@@ -1,11 +1,33 @@
 package com.example.firecloudchat_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SplashScreen : AppCompatActivity() {
+
+    lateinit var spbtnlog:Button
+    lateinit var spbtnreg:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        spbtnlog = findViewById(R.id.splashbtnlogin)
+        spbtnreg = findViewById(R.id.splashbtnreg)
+
+        spbtnlog.setOnClickListener {
+            var gotologin = Intent(this, LoginAtivity::class.java)
+            startActivity(gotologin)
+            finish()
+        }
+
+        spbtnreg.setOnClickListener {
+            var gotoregister = Intent(this, RegisterActvity::class.java)
+            startActivity(gotoregister)
+            finish()
+        }
+
     }
 }
